5b5b935ae40dfd5c159c03987947c6f56fc3d794
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-09-13 01:27:40 -0700

    update spark

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 
 dependencies {
     zenithProxy("com.zenith:ZenithProxy:$mc-SNAPSHOT")
-    shade("me.lucko:spark-common:1.10.134-SNAPSHOT")
+    shade("me.lucko:spark-common:1.10.142-SNAPSHOT")
 }
 
 tasks {
